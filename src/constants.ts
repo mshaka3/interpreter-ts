7905c44371875937dc7d4eadbdc8bc387218af88
mshaka3 <110376803+mshaka3@users.noreply.github.com>
2023-08-12 17:55:25 +0300

    implement_repl (#2)
    Co-authored-by: Mahmoud S <mahmoud.shakaa@creditstacks.com>

---
+++ src/constants.ts
@@ -0,0 +1,38 @@
+import { TokenType } from "./lexer/types";
+
+export const MAP_TOKWN_TYPE_LETIERAL: { [key in string]: TokenType } = {
+    'EOF': 'EOF',
+    '=': 'ASSIGN',
+    'INT': 'INT',
+    '+': 'PLUS',
+    '-': 'MINUS',
+    '*': 'ASTERISK',
+    '!': 'BANG',
+    '/': 'SLASH',
+    '>': 'GT',
+    '<': 'LT',
+    ';': 'SEMICOLON',
+    ',': 'COMMA',
+    '{': 'LBRACE',
+    '}': 'RBRACE',
+    '(': 'LPAREN',
+    ')': 'RPAREN',
+
+    'IDENT': 'IDENT',
+    'ILLEGAL': 'ILLEGAL',
+    'function': 'FUNCTION',
+    'var': 'VAR',
+    'const': 'CONST',
+    'let': 'LET',
+    'else': 'ELSE',
+    'if': 'IF',
+    'return': 'RETURN',
+    'true': 'TRUE',
+    'false': 'FALSE',
+
+    '==': 'EQ',
+    '!=': 'NEQ',
+}
+
+
+
